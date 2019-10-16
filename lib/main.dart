125c6b78c8cb687271a28309a342ef2be6606898
Hossain Md. Arafat <arafat@Hossains-MacBook-Pro.local>
2019-10-16 17:08:05 +0600

    container with icon and row column at same time

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,10 @@ void main(){
 
 class HomePage extends StatelessWidget{
 
+  final _longText = "Hello Arafat. How are You? I am Fine.lakhdka hfka fah fgakcgkahckacash lahlf halh ahf jkafkahkjfhaj falhf ahf hah ";
+  final _shortText = "Hello Arafat.";
+
+
   @override
   Widget build(BuildContext context) {
 
@@ -21,41 +25,39 @@ class HomePage extends StatelessWidget{
         appBar: new AppBar(
           title: new Text('Home'),
         ),
-        body:
-        Column(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(left: 20,top: 20),
-              padding: EdgeInsets.all(20.0) ,
-              height: 40,
-              width: 50,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: Color(Helper.getHexToInt("#F1C40F"))
+        body: Column (children: <Widget>[
+          Container(
+          child: Text(_longText,textAlign: TextAlign.justify,),
+            padding: EdgeInsets.all(20),
+          ),
+          Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(left: 20,top: 20),
+                padding: EdgeInsets.all(5.0) ,
+                height: 40,
+                width: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Color(Helper.getHexToInt("#F1C40F"))
+                ),
+                child: Icon(Icons.linked_camera),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 20,top: 20),
-              padding: EdgeInsets.all(20.0) ,
-              height: 40,
-              width: 50,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: Color(Helper.getHexToInt("#F1C40F"))
+              Container(
+                margin: EdgeInsets.only(left: 20,top: 20),
+                padding: EdgeInsets.all(5.0) ,
+                height: 40,
+                width: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Color(Helper.getHexToInt("#F1C40F"))
+                ),
+                child: Icon(Icons.access_alarm),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 20,top: 20),
-              padding: EdgeInsets.all(20.0) ,
-              height: 40,
-              width: 50,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: Color(Helper.getHexToInt("#F1C40F"))
-              ),
-            )
-          ],
-        ),
+            ],
+          ),
+        ],)
+
       );
   }
 }
