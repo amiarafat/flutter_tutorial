fb7d8c10ebc8a182d27776954398dedcad1eca65
Hossain Md. Arafat <arafat@Hossains-MacBook-Pro.local>
2019-10-17 16:07:38 +0600

    list,listtile

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,22 @@ class HomePage extends StatelessWidget{
   final _longText = "Hello Arafat. How are You? I am Fine.lakhdka hfka fah fgakcgkahckacash lahlf halh ahf jkafkahkjfhaj falhf ahf hah ";
   final _shortText = "Hello Arafat.";
 
+  List people = [
+    {"name":"Arafat", "email":"arafat@gmail.com"},
+    {"name":"Arafat1", "email":"abcd@gmail.com"},
+    {"name":"Arafat2", "email":"efgh@gmail.com"},
+    {"name":"Arafat3", "email":"ijk@gmail.com"},
+    {"name":"Arafat4", "email":"lmn@gmail.com"},
+    {"name":"Arafat5", "email":"opq@gmail.com"},
+    {"name":"Arafat6", "email":"rst@gmail.com"},
+    {"name":"Arafat7", "email":"uvw@gmail.com"},
+    {"name":"Arafat8", "email":"xyz@gmail.com"},
+    {"name":"Arafat9", "email":"arafat@gmail.com"},
+    {"name":"Arafat10", "email":"arafat@gmail.com"},
+    {"name":"Arafat11", "email":"arafat@gmail.com"},
+    {"name":"Arafat12", "email":"arafat@gmail.com"},
+    {"name":"Arafat13", "email":"arafat@gmail.com"},
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -28,52 +44,57 @@ class HomePage extends StatelessWidget{
         body:
         Column(
           children: <Widget>[
-            Stack(
-              children: <Widget>[
-                
-                Image.asset("assets/images/image.jpg"),
-                Positioned(
-                  bottom: 40,
-                  left: 30 ,
-                  child: Text(_shortText,style: TextStyle(color: Colors.white,fontSize: 20)),
+            Container(
+              height: 400,
+              padding: EdgeInsets.all(20),
+              child: ListView(
+                children: <Widget>[
 
-                )
-              ],
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Text("Features",style: TextStyle(color: Colors.orange,fontSize: 20)),
-            SizedBox(
-              height: 30,
+                  _tile(),
+                  Divider(),
+                  _tile(),
+                  Divider(),
+                  _tile(),
+                  Divider(),
+                  _tile(),
+              ],),
+            )
 
-            ),
-            Row(children: <Widget>[
-
-              _rowCall(),
-              _rowCall(),
-              _rowCall(),
-              _rowCall(),
-
-            ],
-            ),
           ],
         )
 
     );
   }
 
-  Widget _rowCall(){
+  Widget _cell(){
 
-    return Expanded(
-        child: Container (
-            height: 50,
-            width: 50,
-            decoration: BoxDecoration(color: Colors.red),
-            margin: EdgeInsets.only(right: 5,left: 5),
-          child: Icon(Icons.link,color: Colors.white,),
+    return Row(
+      children: <Widget>[
+        Container(
+          //margin: EdgeInsets.only(bottom: 10),
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(color: Colors.teal),
+          child: Icon(Icons.lightbulb_outline,color: Colors.white,),
+        ),
+        SizedBox(
+          width: 10,
         )
+      ],
     );
+  }
 
+  Widget _tile(){
+
+    return ListTile(
+      leading: CircleAvatar(
+        child: Text("H"),
+      ),
+      title: Text("Hossain Arafat"),
+      subtitle: Text("New In Flutter"),
+      onTap: (){
+
+      },
+    );
   }
 }
